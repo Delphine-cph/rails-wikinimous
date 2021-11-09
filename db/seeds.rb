836b7bd331407817e1e7484eda19a6bba5679b5c
Delphine ChangPiHin <delphine.cph@gmail.com>
2021-11-09 20:35:13 +0100

    exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 articles...'
+
+10.times do
+  articles = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph(sentence_count: 6)
+  )
+  articles.save!
+end
+
+puts 'Finished!'
